72106138bde240e5caaa1a471172639047fbb3fc
Cody Blackwell <codyblackwell67@gmail.com>
2019-07-10 16:58:45 -0400

    anything

--- Models/leagueModel.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(sequelize, DataTypes){
-    return sequelize.define('league', {
-        wLeague: DataTypes.Array(DataTypes.STRING),
-        qLeague:DataTypes.Array(DataTypes.STRING)
-    })
-}
