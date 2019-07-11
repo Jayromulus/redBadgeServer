3c79374269c4741fee8e69af14fd10f5df0efd56
Jerome Flaherty <jayromulus.f@gmail.com>
2019-07-10 22:18:49 -0400

    portfolio functional

--- controllers/portfolioController.js
+++ controllers/portfolioController.js
@@ -1,6 +1,51 @@
 let router = require('express').Router();
 let Portfolio = require('../db').import('../models/portfolio');
 
-router.get('/get', (req, res) => {
-    console.log('you have accessed the get route')
+router.post('/new', (req, res) => {
+    Portfolio.create({
+        stocks: req.body.portfolio.stocks,
+        owner: req.body.portfolio.owner,
+        league: req.body.portfolio.league,
+        funds: req.body.portfolio.funds
+    }).then(createSucess = (portfolio) => {
+        res.status(200).json({
+            portfolio: portfolio,
+            message: 'Portfolio Created'
+        })
+    },
+    createError = err => res.send(500, err)
+    )
 })
+
+router.get('/', (req, res) => {
+    Portfolio.findAll({where: {owner: 'Keve'}})
+    .then(createSuccess = (portfolio) => {
+        res.status(200).json({
+            portfolio: portfolio,
+            message: 'found them'
+        })
+    },
+    createError = err => res.status(500, err)
+    )
+})
+
+router.put('/update/:id', (req, res) => {
+    Portfolio.update(req.body.portfolio, {
+        where: {id: req.params.id},
+        returning: true
+    }).then(
+        portfolio => res.status(200).json(portfolio)
+    ).catch(
+        err => res.status(500).json({ error: err })
+    )
+})
+
+router.delete('/delete/:id', (req, res) => {
+    Portfolio.destroy({where: {id: req.params.id}}).then(
+        portfolio => res.status(200).json(portfolio)
+    ).catch(
+        err => res.status(500).json({error: err })
+    )
+})
+
+module.exports = router
--- models/portfolio.js
+++ models/portfolio.js
@@ -1,8 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define('portfolio', {
-        stocks: DataTypes.Array(DataTypes.STRING),
+        stocks: DataTypes.ARRAY(DataTypes.STRING),
         owner: DataTypes.STRING,
         league: DataTypes.STRING,
-        funds: DataTypes.REAL
+        funds: DataTypes.FLOAT
     })
 }
