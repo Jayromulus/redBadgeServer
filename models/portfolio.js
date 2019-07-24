28b696bc3bc3f13a31e94f79fd676c4bd1258c01
unknown <oldrustymailbox@gmail.com>
2019-07-24 13:59:25 -0400

    sukit

--- models/portfolio.js
+++ models/portfolio.js
@@ -3,7 +3,8 @@ module.exports = (sequelize, DataTypes) => {
   const Portfolio = sequelize.define('Portfolio', {
     coins: DataTypes.ARRAY(DataTypes.STRING),
     quantity: DataTypes.ARRAY(DataTypes.FLOAT),
-    funds: DataTypes.FLOAT
+    funds: DataTypes.FLOAT,
+    assets: DataTypes.FLOAT
   }, {});
   return Portfolio;
 };
